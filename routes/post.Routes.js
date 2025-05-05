9ddb6f4c0f7d3664582b90da60f94e4694b486cb
daniyal ali <daniyalali12568@gmail.com>
2025-05-05 13:53:01 +0500

    changes_getallpost_routes

--- routes/post.Routes.js
+++ routes/post.Routes.js
@@ -7,7 +7,7 @@ router.post("/create",verifyToken,create)
 router.get("/getpost",getPost);
 router.delete("/deletepost/:postId/:userId",verifyToken,deletePost)
 router.put("/updatepost/:postId/:userId",verifyToken,updatePost)
-router.put("/getAllPost",verifyToken,getALLPost) 
+router.get("/getAllPost",verifyToken,getALLPost) 
 
 // getAllPost
 export default router
