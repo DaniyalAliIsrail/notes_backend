3fd240a14177d813ff0dff2c52a661f1464a6834
daniyal ali <daniyalali12568@gmail.com>
2025-05-05 14:23:25 +0500

    removeVerifyToken_getallpost_routes

--- routes/post.Routes.js
+++ routes/post.Routes.js
@@ -7,7 +7,7 @@ router.post("/create",verifyToken,create)
 router.get("/getpost",getPost);
 router.delete("/deletepost/:postId/:userId",verifyToken,deletePost)
 router.put("/updatepost/:postId/:userId",verifyToken,updatePost)
-router.get("/getAllPost",verifyToken,getALLPost) 
+router.get("/getAllPost",getALLPost) 
 
 // getAllPost
 export default router
